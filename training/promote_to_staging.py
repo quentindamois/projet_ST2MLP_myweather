43d845c58f73f845e979757d8c24bfb378e82b76
quentindamois <quentin.damois@efrei.net>
2026-02-23 02:09:12 +0100

    Update promote_to_staging.py
    modifide the training script to be used in the pipeline and also use alias

--- training/promote_to_staging.py
+++ training/promote_to_staging.py
@@ -1,45 +1,67 @@
 import os
 import json
 import sys
-ACCURACY_THRESHOLD = float(os.getenv("ACCURACY_THRESHOLD", "0.80"))
-
 import os, mlflow
 from mlflow.tracking import MlflowClient
 
-def main():
-    # Accept JSON from stdin or argv for simplicity
-    if not sys.stdin.isatty():
-        payload = sys.stdin.read().strip()
+MAE_THRESHOLD = float(os.getenv("MAE_THRESHOLD", "25000"))
+
+
+def get_latest_metric(metric_history, name):
+    """Get the latest metric for a model"""
+    history_filtered_metric = list(filter(lambda a: a.key == "mae", metric_history))
+    if len(history_filtered_metric) > 0:
+        return sorted(history_filtered_metric, key=lambda a: a.timestamp, reverse=True)[
+            -1
+        ].value
     else:
-        payload = sys.argv[1]
-    data = json.loads(payload)
-    acc = float(data["accuracy"])
-    passed = acc >= ACCURACY_THRESHOLD
+        return 0
+
+
+def main():
+    mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
+    token = os.environ["MLFLOW_TRACKING_TOKEN"]
+    os.environ["MLFLOW_TRACKING_USERNAME"] = token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = token
+    client = MlflowClient()
+    name = os.environ["MODEL_NAME"]
+    alias = os.environ["MODEL_ALIAS"]
+
+    # we get the info on the history of model
+    info_models = client.search_registered_models(filter_string=f"name='{name}'")[0]
+
+    # we get the version correspond to the alias
+    version_of_alias = info_models.aliases[alias]
+
+    # we look for the name of run corresponding to the model name and the version
+    filter_string = f"name='{name}'"
+    results_pagelist = client.search_model_versions(filter_string)
+    # look among the model with the same name for the one with the correct version
+    selected_model_info = list(
+        filter(lambda a: a.version == version_of_alias, results_pagelist)
+    )[0]
+    run_id_of_alias = selected_model_info.run_id
+
+    # We get the mean absolute squared error
+    mae = float(
+        get_latest_metric(client.get_metric_history(run_id_of_alias, "mae"), "mae")
+    )
+
+    passed = mae <= MAE_THRESHOLD
     result = {
         "passed": passed,
-        "accuracy": acc,
-        "threshold": ACCURACY_THRESHOLD,
-        "model_version": data["model_version"],
-        "run_id": data["run_id"],
+        "mae": mae,
+        "threshold": MAE_THRESHOLD,
+        "model_version": version_of_alias,
+        "run_id": run_id_of_alias,
     }
     print(json.dumps(result))
     if not passed:
         sys.exit(2)
     if passed:
-        mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
-        token = os.environ["MLFLOW_TRACKING_TOKEN"]
-        os.environ["MLFLOW_TRACKING_USERNAME"] = token
-        os.environ["MLFLOW_TRACKING_PASSWORD"] = token
-        client = MlflowClient()
-        name = os.environ["MODEL_NAME"]
-        version = data["model_version"]
-        client.transition_model_version_stage(
-            name=name,
-            version=version,
-            stage="Staging",
-            archive_existing_versions=True
-        )
-        print(f"Promoted {name} v{version} to Staging")
+        client.set_registered_model_alias(name, "Staging", version_of_alias)
+        print(f"Promoted {name} v{version_of_alias} to Staging")
+
 
 if __name__ == "__main__":
     main()
