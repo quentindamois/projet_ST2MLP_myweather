a9618082e58fcfe8727f4bfc172e07f5e0227080
quentindamois <quentin.damois@efrei.net>
2026-02-28 19:14:40 +0100

    Create assign_tag.py
    Created a python script to give an alias to a model referenced by another alias

---
+++ training/assign_tag.py
@@ -0,0 +1,30 @@
+import json
+import os
+import sys
+
+import mlflow
+from mlflow.tracking import MlflowClient
+
+def main():
+    mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
+    token = os.environ["MLFLOW_TRACKING_TOKEN"]
+    os.environ["MLFLOW_TRACKING_USERNAME"] = token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = token
+    client = MlflowClient()
+    name = os.environ["MODEL_NAME"]
+    alias = os.environ["MODEL_ALIAS"]
+    new_alias = os.environ["NEW_MODEL_ALIAS"]
+
+    # we get the info on the history of model
+    info_models = client.search_registered_models(filter_string=f"name='{name}'")[0]
+
+    # we get the version correspond to the alias
+    version_of_alias = info_models.aliases[alias]
+    
+    client.set_registered_model_alias(name, "dev", version_of_alias)
+
+    print(f"Add the alias {new_alias} to model {name} v{version_of_alias} that has the alias of {alias}")
+
+
+if __name__ == "__main__":
+    main()
