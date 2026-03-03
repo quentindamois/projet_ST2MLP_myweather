db7f8f13d58333a47d95a3d8d1fb030b7f1fcde3
NoamDetournay <noam.detournay@efrei.net>
2026-03-03 22:30:03 +0100

    alias

--- training/assign_tag.py
+++ training/assign_tag.py
@@ -9,20 +9,34 @@ def main():
     token = os.environ["MLFLOW_TRACKING_TOKEN"]
     os.environ["MLFLOW_TRACKING_USERNAME"] = token
     os.environ["MLFLOW_TRACKING_PASSWORD"] = token
+
     client = MlflowClient()
+
     name = os.environ["MODEL_NAME"]
     alias = os.environ["MODEL_ALIAS"]
     new_alias = os.environ["NEW_MODEL_ALIAS"]
 
-    # we get the info on the history of model
-    info_models = client.search_registered_models(filter_string=f"name='{name}'")[0]
+    # Get registered model
+    info_models = client.search_registered_models(
+        filter_string=f"name='{name}'"
+    )[0]
 
-    # we get the version correspond to the alias
-    version_of_alias = info_models.aliases[alias]
+    # If alias exists → use it
+    if alias in info_models.aliases:
+        version = info_models.aliases[alias]
+        print(f"Alias {alias} exists → using version {version}")
+    else:
+        # Otherwise → take latest version
+        latest_versions = client.get_latest_versions(name)
+        version = latest_versions[0].version
+        print(
+            f"Alias {alias} does not exist → using latest version {version}"
+        )
 
-    client.set_registered_model_alias(name, "dev", version_of_alias)
+    # Set new alias
+    client.set_registered_model_alias(name, new_alias, version)
 
-    print(f"Add the alias {new_alias} to {alias} of {name} v{version_of_alias}")
+    print(f"Set alias {new_alias} to version {version} of model {name}")
 
 
 if __name__ == "__main__":
